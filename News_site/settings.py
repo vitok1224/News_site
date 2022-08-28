c755d7c24364c65737be60dced2b7a19beb7a616
vitok1224 <98743825+vitok1224@users.noreply.github.com>
2022-08-28 14:51:26 +0300

    Update settings.py

--- News_site/settings.py
+++ News_site/settings.py
@@ -79,13 +79,7 @@ WSGI_APPLICATION = 'News_site.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'News_site',
-        'USER': 'root',
-        'PASSWORD': '',
-        'PORT': '3306',
-        'HOST': 'localhost'
+    
     }
 }
 
